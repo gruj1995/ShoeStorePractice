f61be838baea47a46a1ecf020a96cf7b37aea21d
developer <gruj1995@berich.net.tw>
2023-05-13 08:26:01 +0800

    feat: 建立專案

---
+++ ShoeStorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoeStorePractice
+//
+//  Created by 李品毅 on 2023/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
